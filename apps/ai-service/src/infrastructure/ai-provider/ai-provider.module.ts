2b402a700ee79aa5cc408ca5f8b5cc93cc4b5bd7
Stas Tarnoguskyi <stas.tarnoguskiy@avasure.com>
2026-05-28 20:06:31 +0300

    fix: exp 1.2

--- apps/ai-service/src/infrastructure/ai-provider/ai-provider.module.ts
+++ apps/ai-service/src/infrastructure/ai-provider/ai-provider.module.ts
@@ -1,23 +1,25 @@
 import { Module } from '@nestjs/common'
-import { ConfigService } from '@nestjs/config'
 
 import { AiProvider } from '@/common/abstractions/ai-provider.abstract'
-import { AllConfigs } from '@/config/interfaces'
 
 import { LMStudioLocalProvider } from './providers/lm-studio-local.provider'
 
 @Module({
 	providers: [
+		// {
+		// 	provide: AiProvider,
+		// 	useFactory: (config: ConfigService<AllConfigs>) => {
+		// 		// const env = config.get('app.node_env', { infer: true })
+		// 		// return env === Environment.Production
+		// 		// 	? new OpenrouterProvider(config)
+		// 		// 	: new LMStudioLocalProvider(config)
+		// 		new LMStudioLocalProvider(config)
+		// 	},
+		// 	inject: [ConfigService]
+		// }
 		{
 			provide: AiProvider,
-			useFactory: (config: ConfigService<AllConfigs>) => {
-				// const env = config.get('app.node_env', { infer: true })
-				// return env === Environment.Production
-				// 	? new OpenrouterProvider(config)
-				// 	: new LMStudioLocalProvider(config)
-				new LMStudioLocalProvider(config)
-			},
-			inject: [ConfigService]
+			useClass: LMStudioLocalProvider
 		}
 	],
 	exports: [AiProvider]
