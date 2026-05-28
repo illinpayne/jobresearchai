f6e9b249edbb6191e9cabaed30840f6fe682c324
Stas Tarnoguskyi <stas.tarnoguskiy@avasure.com>
2026-05-28 20:01:42 +0300

    fix: experimental ai fix

--- apps/ai-service/src/infrastructure/ai-provider/ai-provider.module.ts
+++ apps/ai-service/src/infrastructure/ai-provider/ai-provider.module.ts
@@ -3,20 +3,19 @@ import { ConfigService } from '@nestjs/config'
 
 import { AiProvider } from '@/common/abstractions/ai-provider.abstract'
 import { AllConfigs } from '@/config/interfaces'
-import { Environment } from '@/config/validators'
 
 import { LMStudioLocalProvider } from './providers/lm-studio-local.provider'
-import { OpenrouterProvider } from './providers/openrouter.provider'
 
 @Module({
 	providers: [
 		{
 			provide: AiProvider,
 			useFactory: (config: ConfigService<AllConfigs>) => {
-				const env = config.get('app.node_env', { infer: true })
-				return env === Environment.Production
-					? new OpenrouterProvider(config)
-					: new LMStudioLocalProvider(config)
+				// const env = config.get('app.node_env', { infer: true })
+				// return env === Environment.Production
+				// 	? new OpenrouterProvider(config)
+				// 	: new LMStudioLocalProvider(config)
+				new LMStudioLocalProvider(config)
 			},
 			inject: [ConfigService]
 		}
