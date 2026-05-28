0c33afa3785c5b63b598597be0d70136eced5e24
Stas Tarnoguskyi <stas.tarnoguskiy@avasure.com>
2026-05-28 11:10:12 +0300

    fix: update prisma connection

--- apps/ai-core-service/src/infrastructure/prisma/prisma.service.ts
+++ apps/ai-core-service/src/infrastructure/prisma/prisma.service.ts
@@ -16,7 +16,7 @@ export class PrismaService
 
 	constructor() {
 		const adapter = new PrismaPg({
-			connectionString: process.env.DATABASE_URL as string
+			connectionString: process.env.AICORE_DATABASE_URL as string
 		})
 		super({ adapter })
 	}
